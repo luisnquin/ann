8aa8c8f9e9b013d22d7dfe3b984da7da9b72e5c1
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-03-20 23:28:33 -0500

    feat: first draft

---
+++ cmd/ann/main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/d-tsuji/clipboard"
+	"github.com/google/uuid"
+	"github.com/marcusolsson/tui-go"
+	gonanoid "github.com/matoous/go-nanoid/v2"
+)
+
+type tool struct {
+	name string
+	task func() (string, error)
+}
+
+func main() {
+	faker := gofakeit.New(time.Now().Unix())
+
+	tools := []tool{
+		{
+			name: "UUID",
+			task: func() (string, error) {
+				uuid := uuid.NewString()
+
+				return uuid, clipboard.Set(uuid)
+			},
+		},
+		{
+			name: "Nano ID",
+			task: func() (string, error) {
+				nanoid := gonanoid.Must()
+
+				return nanoid, clipboard.Set(nanoid)
+			},
+		},
+		{
+			name: "Email",
+			task: func() (string, error) {
+				email := faker.Email()
+
+				return email, clipboard.Set(email)
+			},
+		},
+		{
+			name: "Full name",
+			task: func() (string, error) {
+				p := faker.Person()
+
+				fullName := fmt.Sprintf("%s %s", p.FirstName, p.LastName)
+
+				return fullName, clipboard.Set(fullName)
+			},
+		},
+		{
+			name: "Username",
+			task: func() (string, error) {
+				username := faker.Username()
+
+				return username, clipboard.Set(username)
+			},
+		},
+		{
+			name: "Phone number",
+			task: func() (string, error) {
+				phoneNumber := faker.Phone()
+
+				return phoneNumber, clipboard.Set(phoneNumber)
+			},
+		},
+		{
+			name: "Credit card",
+			task: func() (string, error) {
+				creditCard := faker.CreditCardNumber(&gofakeit.CreditCardOptions{
+					Types: []string{"visa", "mastercard"},
+					Gaps:  true,
+				})
+
+				return creditCard, clipboard.Set(creditCard)
+			},
+		},
+		{
+			name: "Company",
+			task: func() (string, error) {
+				company := faker.Company()
+
+				return company, clipboard.Set(company)
+			},
+		},
+		{
+			name: "Phrase",
+			task: func() (string, error) {
+				phrase := faker.Phrase()
+
+				return phrase, clipboard.Set(phrase)
+			},
+		},
+	}
+
+	generators := tui.NewTable(0, 0)
+	// library.SetColumnStretch(0, 1)
+	// library.SetColumnStretch(1, 1)
+	// library.SetColumnStretch(2, 4)
+
+	generators.SetFocused(true)
+
+	generators.AppendRow(
+		tui.NewLabel("What do you want to generate?"),
+	)
+
+	for _, tool := range tools {
+		label := tui.NewLabel(tool.name)
+
+		generators.AppendRow(
+			label,
+		)
+	}
+
+	currentClipboardText, err := clipboard.Get()
+	if err != nil {
+		panic(err)
+	}
+
+	statusLeft, statusRight := tui.NewStatusBar(""), tui.NewStatusBar("")
+
+	// This implies a weird behavior that pushes the text to the right side of the HBox
+	statusRight.SetPermanentText("Ninym Ralei the best possible girl")
+
+	setClipboardStatus := func(s string) { statusLeft.SetText(fmt.Sprintf("Clipboard: %s", s)) }
+	setClipboardStatus(currentClipboardText)
+
+	statuses := tui.NewHBox(statusLeft, statusRight)
+	statuses.SetBorder(true)
+
+	root := tui.NewVBox(
+		generators,
+		tui.NewSpacer(),
+		statuses,
+	)
+
+	ui, err := tui.New(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	generators.OnItemActivated(func(t *tui.Table) {
+		if t.Selected() == 0 {
+			return
+		}
+
+		out, err := tools[t.Selected()-1].task()
+		if err != nil {
+			panic(err)
+		}
+
+		setClipboardStatus(out)
+	})
+
+	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	ui.SetKeybinding("q", func() { ui.Quit() })
+
+	theme := tui.NewTheme()
+	theme.SetStyle("table.cell.selected", tui.Style{
+		Bold: tui.DecorationOn, Reverse: tui.DecorationOn,
+		Fg: tui.ColorBlue, Bg: tui.ColorWhite,
+	})
+
+	ui.SetTheme(theme)
+
+	if err := ui.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/luisnquin/ann
+
+go 1.20
+
+require (
+	github.com/brianvoe/gofakeit/v6 v6.20.2
+	github.com/d-tsuji/clipboard v0.0.3
+	github.com/google/uuid v1.3.0
+	github.com/marcusolsson/tui-go v0.4.0
+	github.com/matoous/go-nanoid/v2 v2.0.0
+)
+
+require (
+	git.wow.st/gmp/clip v0.0.0-20191001134149-1458ba6a7cf5 // indirect
+	github.com/BurntSushi/xgb v0.0.0-20200324125942-20f126ea2843 // indirect
+	github.com/gdamore/encoding v0.0.0-20151215212835-b23993cbb635 // indirect
+	github.com/gdamore/tcell v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v0.0.0-20180709185858-c7842319cf3a // indirect
+	github.com/lxn/walk v0.0.0-20191128110447-55ccb3a9f5c1 // indirect
+	github.com/lxn/win v0.0.0-20191128105842-2da648fda5b4 // indirect
+	github.com/mattn/go-runewidth v0.0.3 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+	gopkg.in/Knetic/govaluate.v3 v3.0.0 // indirect
+)
